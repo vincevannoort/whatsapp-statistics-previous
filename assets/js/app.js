ec18517c6f7e3d98b99caddf40e3c0f1696ee978
Vince van Noort <vince.vannoort@student.hu.nl>
2017-07-01 23:22:06 +0200

    Implemented pasring for emojis using regex

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,7 +16,35 @@ var app = new Vue({
         }
         return all_persons;
       }, {});
-      return counted_lines;
+      return _.fromPairs(_.sortBy(_.toPairs(counted_lines), function(a){return a[1]}).reverse());
+    },
+    statistics_emoji_count: function() {
+      var counted_emojis = this.lines.reduce(function (all_emojis, line) {
+        var emoji_array = line.line.match(/([\uD800-\uDBFF][\uDC00-\uDFFF])/g);
+        if (emoji_array != null) {
+          emoji_array.forEach(function(emoji){
+            if (emoji in all_emojis) {
+              all_emojis[emoji]++;
+            } else {
+              all_emojis[emoji] = 1;
+            }
+          });
+        }
+        return all_emojis;
+      }, {});
+
+      // limit to 10 emoji's
+      var ordered_counted_emojis = _.fromPairs(_.sortBy(_.toPairs(counted_emojis), function(a){return a[1]}).reverse());
+      var emojis = {};
+      var count = 0;
+      for (emoji in ordered_counted_emojis) {
+        if (count < 10) {
+          emojis[emoji] = ordered_counted_emojis[emoji];
+          count++;
+        } else { break; }
+      }
+
+      return emojis;
     }
   },
   methods: {
@@ -37,6 +65,8 @@ var app = new Vue({
           person = line.substr(timelength+2, line.substr(personlength).indexOf(":"));
           if (!isNaN(date) && person != '') { return line; }
         }).map(function(line) {
+          
+          // return object split by date, time, person and line
           return {
             date: line.substr(0, datelength),
             time: line.substr(datelength+1, timelength),
